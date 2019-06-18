f735f41a13bed9346e6749337871831fc286b2a2
Neha Patel <nehatr0n@Nehas-MacBook-Pro.local>
2019-06-18 15:09:51 -0700

    bus alert honk and nav items change size over hover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+const funBusImg = document.querySelector('header img');
+funBusImg.addEventListener('click', (event) => {
+    alert("HONK HONK!");
+});
+
+const navLink = document.querySelectorAll('.nav-link');
+navLink.forEach((el, index) =>
+    el.addEventListener('mouseover', (event)=> {
+        el.style.fontSize = "2rem";
+
+        setTimeout(function() {
+            el.style.fontSize = "";
+          }, 400);
+        }, false)
+);
